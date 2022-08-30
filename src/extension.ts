4c48cc52deb035a74a2e2f7200d54b6f381443bb
全卓 <quanzhuo@kylinos.cn>
2022-08-30 18:55:32 +0800

    language server startup

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as path from 'path';
 import * as vscode from 'vscode';
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cmake-ls" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cmake-ls.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CMake IntelliSence!');
-	});
+    const config = vscode.workspace.getConfiguration('cmakeIntelliSence');
+    let cmakels = config.get<string>('languageServerPath');
+    if (cmakels === undefined) {
+        cmakels = 'cmakels';
+    }
 
-	context.subscriptions.push(disposable);
+    // language server options
+    // default transport is TransportKind.stdio
+    const serverOptions: ServerOptions = {
+        command: cmakels,
+        // args: [serverEntryPath]
+    };
+
+    // Options to control the language client
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [
+            { language: 'cmake', scheme: 'file' },
+            { language: 'cmake', scheme: 'untitled' }
+        ],
+        outputChannel: vscode.window.createOutputChannel('CMake IntelliSence')
+    };
+
+    // Create the language client and start the client.
+    client = new LanguageClient(
+        'cmakeLanguageServer',
+        'CMake Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+    // start the client. This will also launch the server
+    client.start();
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
